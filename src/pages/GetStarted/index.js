6ef20470f34c9c9cc26e72c527ae0859886b4390
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-16 16:50:03 +0700

    add react navigation

--- src/pages/GetStarted/index.js
+++ src/pages/GetStarted/index.js
@@ -4,7 +4,7 @@ import { ILGetStarted, ILLogo } from '../../assets'
 import Button from '../../components/atoms/Button'
 import Gap from '../../components/atoms/Gap'
 
-const GetStarted = () => {
+const GetStarted = ({navigation}) => {
   return (
     <ImageBackground source={ILGetStarted} style={styles.page}>
       <View>
@@ -16,9 +16,16 @@ const GetStarted = () => {
         </Text>
       </View>
       <View>
-        <Button title="Get Started" />
+        <Button 
+          title="Get Started" 
+          onPress={() => navigation.navigate('Register')} 
+        />
         <Gap height={16} />
-        <Button type="secondary" title="Sign In" />
+        <Button 
+          type="secondary" 
+          title="Sign In" 
+          onPress={() => navigation.navigate('Login')}
+        />
       </View>
     </ImageBackground>
   )
