d0b5ed7c23c375938e28b69565f689c8797de293
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-18 21:48:35 +0700

    create login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,45 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { ILLogo } from '../../assets'
+import { Button, Gap, Input, Link } from '../../components'
+import { colors, fonts } from '../../utils'
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.page}>
+          <ILLogo />
+          <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
+          <Input label="Email Address" />
+          <Gap height={24} />
+          <Input label="Password" />
+          <Gap height={10} />
+          <Link title="Forgot My Password" size={12} />
+          <Gap height={40} />
+          <Button title="Sign In" />
+          <Gap height={30} />
+          <Link title="Create New Account" size={16} align="center" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    padding: 40,
+    flex: 1,
+    backgroundColor: colors.white
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 40,
+    marginBottom: 40,
+    maxWidth: 153
+  }
+})
