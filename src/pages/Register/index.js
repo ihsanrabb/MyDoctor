6fd8d2b2a02b9460b96413c4d835fe96fbb8cd12
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-18 21:48:54 +0700

    create register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,14 +1,43 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, ScrollView ,StyleSheet, View } from 'react-native'
+import { Button, Header, Input, Gap } from '../../components'
+import { colors } from '../../utils'
 
-const Register = () => {
+const Register = ({navigation}) => {
   return (
-    <View>
-      <Text>register</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.page}>
+          <Header onPress={() => navigation.goBack()} title="Daftar Akun" />
+          <View style={styles.content}>
+            <Input label="Full Name" />
+            <Gap height={24} />
+            <Input label="Pekerjaan" />
+            <Gap height={24} />
+            <Input label="Email Address" />
+            <Gap height={24} />
+            <Input label="Password" />
+            <Gap height={40} />
+            <Button 
+              title="Continue" 
+              onPress={() => navigation.navigate('UploadPhoto')}  
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default Register
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.white,
+    flex: 1
+  },
+  content: {
+    padding: 40,
+    paddingTop: 0
+  }
+})
