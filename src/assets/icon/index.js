8eb58805efb9b1f361610d119ca27aeb6f8082ab
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-18 21:44:45 +0700

    add icon

---
+++ src/assets/icon/index.js
@@ -0,0 +1,9 @@
+import IconBackDark from './ic-back-dark.svg'
+import IconAddPhoto from './ic-add-photo.svg'
+import IconRemovePhoto from './ic-remove-photo.svg'
+
+export {
+    IconBackDark,
+    IconAddPhoto,
+    IconRemovePhoto
+}
