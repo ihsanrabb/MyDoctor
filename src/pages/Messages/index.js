3157ebc663dbdbb73626f10898df0f649cb04c04
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-24 18:01:38 +0700

    useState for store data

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { DummyDoctor1, DummyDoctor2, DummyDoctor3 } from '../../assets'
 import { ListDoctor } from '../../components'
 import { colors, fonts } from '../../utils'
 
 const Messages = () => {
+  const [doctors] = useState([
+    {
+      id: 1,
+      profile: DummyDoctor1,
+      name: 'Ihsanuddin',
+      desc: 'Baik pak terima kasih...'
+    },
+    {
+      id: 2,
+      profile: DummyDoctor2,
+      name: 'Cima',
+      desc: 'Oh tentu saja tidak bisa'
+    },
+    {
+      id: 3,
+      profile: DummyDoctor3,
+      name: 'gatnes',
+      desc: 'au ah elap'
+    }
+  ])
   return (
     <View style={styles.page}>
       <View style={styles.content}>
         <Text style={styles.title}>Messages</Text>
-        <ListDoctor />
-        <ListDoctor />
-        <ListDoctor />
+        {
+          doctors.map(doctor => {
+            return (
+              <ListDoctor 
+                key={doctor.id}
+                profile={doctor.profile} 
+                name={doctor.name} 
+                desc={doctor.desc}
+              /> 
+            )
+          })
+        }
       </View>
     </View>
   )
