a06d4a913fb7bedd78942ed8ef5c3fcd03fd0a29
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-24 16:42:34 +0700

    imoprt export molecules component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,5 +1,19 @@
 import Header from './Header'
+import BottomNavigator from './BottomNavigator'
+import HomeProfile from './HomeProfile'
+import DoctorCategory from './DoctorCategory'
+import RatedDoctor from './RatedDoctor'
+import NewsItem from './NewsItem'
+import ListDoctor from './ListDoctor'
+import ListHospitals from './ListHospitals'
 
 export {
-    Header
+    Header,
+    BottomNavigator,
+    HomeProfile,
+    DoctorCategory,
+    RatedDoctor,
+    NewsItem,
+    ListDoctor,
+    ListHospitals
 }
