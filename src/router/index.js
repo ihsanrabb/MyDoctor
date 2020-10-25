3a24f43a810c30e7222dcf356d89e4a1a3b272d0
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-25 19:27:41 +0700

    add choose doctor and chatting page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import { GetStarted, Login, Register, Splash, UploadPhoto, Doctor, Messages, Hospitals } from '../pages'
+import { 
+  GetStarted, 
+  Login, 
+  Register, 
+  Splash, 
+  UploadPhoto, 
+  Doctor, 
+  Messages, 
+  Hospitals,
+  ChooseDoctor,
+  Chatting 
+} from '../pages'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { BottomNavigator } from '../components';
 
@@ -48,6 +59,16 @@ const Router = () => {
       <Stack.Screen 
         name="MainApp" 
         component={MainApp} 
+        options={{headerShown: false}} 
+      />
+      <Stack.Screen 
+        name="ChooseDoctor" 
+        component={ChooseDoctor} 
+        options={{headerShown: false}} 
+      />
+      <Stack.Screen 
+        name="Chatting" 
+        component={Chatting} 
         options={{headerShown: false}} 
       />
     </Stack.Navigator>
