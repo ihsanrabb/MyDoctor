61cbd9787e8f5ea29a59a78b4629dcb70595c79b
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-28 15:08:14 +0700

    handle onPress & add dynamic data

--- src/components/molecules/RatedDoctor/index.js
+++ src/components/molecules/RatedDoctor/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
-import { DummyDoctor1, IconStar } from '../../../assets'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import { IconStar } from '../../../assets'
 import { colors, fonts } from '../../../utils'
 
-const RatedDoctor = () => {
+const RatedDoctor = (props) => {
   return (
-    <View style={styles.container} >
-      <Image source={DummyDoctor1} style={styles.avatar} />
+    <TouchableOpacity style={styles.container} onPress={props.onPress}>
+      <Image source={props.avatar} style={styles.avatar} />
       <View style={styles.profile}>
-        <Text style={styles.name} >Alex Rachel</Text>
-        <Text style={styles.category}>Dentist</Text>
+        <Text style={styles.name}>{props.name}</Text>
+        <Text style={styles.category}>{props.desc}</Text>
       </View>
       <View style={styles.rate}>
         <IconStar />
@@ -18,7 +18,7 @@ const RatedDoctor = () => {
         <IconStar />
         <IconStar />
       </View>
-    </View>
+    </TouchableOpacity>
   )
 }
 
@@ -28,7 +28,8 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingBottom: 16
+    paddingBottom: 16,
+    alignItems: 'center'
   },  
   avatar: {
     width: 50,
