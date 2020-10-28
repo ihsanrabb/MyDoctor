4b86c058694d2d134e5c20eeeef0c8a3c55c2bd1
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-28 15:09:46 +0700

    List handle onPress

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { DummyDoctor1, DummyDoctor2, DummyDoctor3 } from '../../assets'
-import { ListDoctor } from '../../components'
+import { List } from '../../components'
 import { colors, fonts } from '../../utils'
 
-const Messages = () => {
+const Messages = ({navigation}) => {
   const [doctors] = useState([
     {
       id: 1,
@@ -32,11 +32,12 @@ const Messages = () => {
         {
           doctors.map(doctor => {
             return (
-              <ListDoctor 
+              <List 
                 key={doctor.id}
                 profile={doctor.profile} 
                 name={doctor.name} 
                 desc={doctor.desc}
+                onPress={() => navigation.navigate('Chatting')}
               /> 
             )
           })
