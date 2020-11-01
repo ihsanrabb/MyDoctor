9da5241f89f1b73aa7f24ee9643d38778f78ef5b
ihsanrabb <ihsanrabbani25@gmail.com>
2020-11-01 21:27:04 +0700

    add black and red color

--- src/utils/Colors/index.js
+++ src/utils/Colors/index.js
@@ -7,7 +7,10 @@ const mainColors = {
   grey1 : '#7D8797',
   grey2 : '#e9e9e9',
   grey3: '#EDEEF0',
-  blue1: '#0066CB'
+  blue1: '#0066CB',
+  black1: '#000',
+  black2: 'rgba(0, 0, 0, 0.5)',
+  red1: '#E06379'
 }
 
 export const colors = {
@@ -35,5 +38,7 @@ export const colors = {
     }
   },
   border: mainColors.grey2,
-  cardLight: mainColors.green2
+  cardLight: mainColors.green2,
+  loadingBackground: mainColors.black2,
+  error: mainColors.red1
 }
