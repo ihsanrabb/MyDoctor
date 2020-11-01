c3013cc63d5bdf939f735d26842d3c3330077384
ihsanrabb <ihsanrabbani25@gmail.com>
2020-11-01 21:26:23 +0700

    implement firebase

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,31 +1,82 @@
-import React from 'react'
-import { SafeAreaView, ScrollView ,StyleSheet, View } from 'react-native'
-import { Button, Header, Input, Gap } from '../../components'
-import { colors } from '../../utils'
+import React, { useState } from 'react'
+import { ScrollView ,StyleSheet, View } from 'react-native'
+import { Button, Header, Input, Gap, Loading } from '../../components'
+import { colors, useForm } from '../../utils'
+import { Fire } from '../../config'
+import { showMessage, hideMessage } from "react-native-flash-message";
 
 const Register = ({navigation}) => {
+  const [form, setForm] = useForm({
+    fullName: '',
+    profession: '',
+    email: '',
+    password: ''
+  }) 
+  const [loading, setLoading] = useState(false)
+
+  const onContinue = () => {
+    console.log('masok', form)
+
+    setLoading(true)
+    Fire.auth().createUserWithEmailAndPassword(form.email, form.password)
+      .then((res) => {
+        console.log('register success', res)
+        setLoading(false)
+        setForm('reset')
+      })
+      .catch((error) => {
+        let errorMessage = error.message;
+        showMessage({
+          message: errorMessage,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white
+        })
+        console.log('error register', errorMessage)
+        setLoading(false)
+      });
+  }
+
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.page}>
-          <Header onPress={() => navigation.goBack()} title="Daftar Akun" />
-          <View style={styles.content}>
-            <Input label="Full Name" />
-            <Gap height={24} />
-            <Input label="Pekerjaan" />
-            <Gap height={24} />
-            <Input label="Email Address" />
-            <Gap height={24} />
-            <Input label="Password" />
-            <Gap height={40} />
-            <Button 
-              title="Continue" 
-              onPress={() => navigation.navigate('UploadPhoto')}  
-            />
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <View style={styles.page}>
+        <Header onPress={() => navigation.goBack()} title="Daftar Akun" />
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.content}>
+              <Input 
+                label="Full Name" 
+                value={form.fullName} 
+                onChangeText={(val) => setForm('fullName', val)} 
+              />
+              <Gap height={24} />
+              <Input 
+                label="Pekerjaan" 
+                value={form.profession}
+                onChangeText={(val) => setForm('profession', val)}  
+              />
+              <Gap height={24} />
+              <Input 
+                label="Email Address" 
+                value={form.email}
+                onChangeText={(val) => setForm('email', val)}  
+              />
+              <Gap height={24} />
+              <Input 
+                label="Password" 
+                value={form.password}
+                onChangeText={(val) => setForm('password', val)}  
+                secureTextEntry
+              />
+              <Gap height={40} />
+              <Button 
+                title="Continue" 
+                onPress={onContinue}  
+              />
+            </View>
+          </ScrollView>
+      </View>
+      {loading && <Loading />}
+    </>
   )
 }
 
